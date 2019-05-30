bd17e35bc9fe777eef9d00c1e66e8dd055a0884b
Ian Carlos <carlos.ian007@gmail.com>
2019-05-30 00:41:21 -0300

    Style(header): Add header

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import GlobalStyle from './styles/global';
 
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
+import Header from './components/Header';
 
-import { Wrapper, Container } from './styles/components';
+import { Wrapper, Container, Content } from './styles/components';
 
 const App = () => (
   <>
@@ -12,6 +13,9 @@ const App = () => (
     <Wrapper>
       <Container>
         <Sidebar />
+        <Content>
+          <Header />
+        </Content>
       </Container>
       <Player />
     </Wrapper>
---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Container, Search, User } from './styles';
+
+const Header = () => (
+  <Container>
+    <Search>
+      <input placeholder="Search" />
+    </Search>
+
+    <User>
+      <img
+        src="https://avatars1.githubusercontent.com/u/15328398?v=4"
+        alt="User"
+      />
+      Ian Carlos
+    </User>
+  </Container>
+);
+
+export default Header;
